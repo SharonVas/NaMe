c25ff27487d025ac1b63f408cb9e6d967a79eaf7
svnb2 <svnb2011@gmail.com>
2018-08-10 20:13:51 +0300

    first commit- empty project

---
+++ app/src/androidTest/java/com/svnb2/name/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.svnb2.name;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.svnb2.name", appContext.getPackageName());
+    }
+}
