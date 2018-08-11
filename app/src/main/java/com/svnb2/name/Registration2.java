4a5ee8bb00aa896385541d97e3fec1767cda61e0
svnb2 <svnb2011@gmail.com>
2018-08-11 15:52:04 +0300

    Registration screen1 Ui+Functional

---
+++ app/src/main/java/com/svnb2/name/Registration2.java
@@ -0,0 +1,21 @@
+package com.svnb2.name;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Registration2 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view= inflater.inflate(R.layout.activity_registration1,container,false);
+        return view;
+    }
+}
