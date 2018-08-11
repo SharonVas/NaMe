99fb1378157090ae708580682897890841bff886
svnb2 <svnb2011@gmail.com>
2018-08-11 03:39:22 +0300

    Registration_1_1.00

--- app/src/main/java/com/svnb2/name/Registration1.java
+++ app/src/main/java/com/svnb2/name/Registration1.java
@@ -1,5 +1,6 @@
 package com.svnb2.name;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class Registration1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration1);
+        getSupportActionBar().setTitle("Registration");
     }
+
+
 }
